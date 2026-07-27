539790eeb8cfb10ceaf5f857ab1c93c42dac0db1
Gustavo <gustavogod007boss@gmail.com>
2026-07-27 15:37:15 -0600

    Agrega Header y configura rutas de la aplicación con React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+ import { BrowserRouter, Route, Routes } from "react-router-dom"
+ import { Toaster } from "react-hot-toast" 
+ import Header from "./components/Header"
+ import AlumnoList from "./components/alumnos/AlumnoList"
+ import AlumnoForm from "./components/alumnos/AlumnoForm" 
+     
+ import CarreraList from "./components/carreras/CarreraList" 
+ import CarreraForm from "./components/carreras/CarreraForm" 
+ import MateriaList from "./components/materias/MateriaList"
+ import MateriaForm from "./components/materias/MateriaForm"
+ import InscripcionList from "./components/inscripciones/InscripcionList"
+ import InscripcionForm from "./components/inscripciones/InscripcionForm" 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     
-    </>
-  )
+    <BrowserRouter>
+      <div className="container mx-auto">
+        <Header />
+        <Routes>
+          <Route path="/" element={<AlumnoList />} />
+          <Route path="/nuevo-alumno" element={<AlumnoForm />} />
+          <Route path="/editar-alumno/:id" element={<AlumnoForm />} />
+
+          <Route path="/carreras" element={<CarreraList />} />
+          <Route path="/nueva-carrera" element={<CarreraForm />} />
+          <Route path="/editar-carrera/:id" element={<CarreraForm />} />
+
+          <Route path="/materias" element={<MateriaList />} />
+          <Route path="/nueva-materia" element={<MateriaForm />} />
+          <Route path="/editar-materia/:id" element={<MateriaForm />} />
+
+          <Route path="/inscripciones" element={<InscripcionList />} />
+          <Route path="/nueva-inscripcion" element={<InscripcionForm />} />
+          <Route path="/editar-inscripcion/:id" element={<InscripcionForm />} />
+        </Routes>
+        <Toaster />
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
